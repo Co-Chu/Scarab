976ada175086189f23823ff44a214a630b5e73b3
Matthew Lanigan <rintaun@gmail.com>
2018-08-24 18:37:54 -0400

    Remove scarab/scarab.rb

--- lib/scarab.rb
+++ lib/scarab.rb
@@ -1,3 +1,14 @@
 # frozen_string_literal: true
 
-require 'scarab/scarab'
+# Scarab
+#
+# Scarab is a lightweight web routing framework built on Sinatra, similar to
+# Sinatra's "namespace" plugin.
+#
+# @author Matthew Lanigan <rintaun@gmail.com>
+# @since 1.0.0
+module Scarab
+    require 'scarab/app'
+    require 'scarab/controller'
+    require 'scarab/version'
+end
