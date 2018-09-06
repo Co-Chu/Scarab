1e9a8b3d391adc43b75c20fd47a6b405cf240e54
Matthew Lanigan <rintaun@gmail.com>
2018-09-05 23:24:36 -0400

    Instantiate controllers at request time
    This is undoubtedly less performant than the way Sinatra normally does 
    it, but there doesn't appear to be a way to maintain the middleware 
    stack *and* also notify the controller of the current request's app 
    instance. Brief testing didn't show that big a difference in 
    performance, and given that the applications are generally quite 
    lightweight, I don't foresee many problems as a result of this approach.

--- lib/scarab/app.rb
+++ lib/scarab/app.rb
@@ -21,21 +21,14 @@ module Scarab
 
         def self.setup_logging(builder); end
 
-        def initialize(app = nil)
-            super
-            @controllers = self.class.controllers.map do |controller|
-                controller.new(self)
-            end
-        end
-
-        attr_reader :controllers
-
         # Passes control to any controllers for which the route prefix matches
         # the requested path. Reverts to App-level control if the controller
         # passes or has no matching route.
         def route!(base = settings, pass_block = nil)
             return super unless base == settings
-            controllers.each { |controller| check_controller!(controller) }
+            self.class.controllers.each do |controller|
+                check_controller!(controller)
+            end
             super
         end
 
@@ -44,11 +37,7 @@ module Scarab
             if (pattern = controller.settings.route_pattern)
                 return unless pattern === request.path_info
             end
-            catch(:pass) do
-                controller.dup.tap do |c|
-                    c.app = self
-                end.call!(env)
-            end
+            catch(:pass) { controller.new(self).call(env) }
         end
     end
 end
